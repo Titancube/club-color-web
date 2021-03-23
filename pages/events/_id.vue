b57527d5b4c3286f70872bc2aec59b07d6e2574a
titancube <kbin0717@gmail.com>
2021-03-24 03:16:05 +0900

    add: events component

---
+++ pages/events/_id.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    class="container flex flex-wrap justify-center items-start min-h-screen w-5/6 mx-auto"
+  >
+    <div class="w-full flex justify-center items-center">
+      <img
+        class="w-full h-auto"
+        src="https://via.placeholder.com/500x800"
+        alt=""
+      />
+    </div>
+    <div class="w-full">
+      <p>awef</p>
+      <p>awef</p>
+      <p>awef</p>
+      <p>awef</p>
+      <p>awef</p>
+    </div>
+    <div class="flex justify-center">
+      <nuxt-link
+        class="mt-4 text-2xl font-bold text-gray-300 text-center mx-auto"
+        to="/events"
+        >Back</nuxt-link
+      >
+    </div>
+  </div>
+</template>
+
+<script lang='ts'>
+import { Component, Vue, Prop } from "nuxt-property-decorator";
+
+@Component
+export default class id extends Vue {
+  scrollToTop: true;
+}
+</script>
+
+<style scoped lang='postcss'>
+</style>
--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,14 +1,38 @@
 <template>
   <div
-    class="container mx-auto flex justify-center items-center h-screen"
-  ></div>
+    class="container mx-auto w-5/6 md:w-full flex flex-wrap justify-center items-start min-h-screen py-4"
+  >
+    <div
+      class="relative w-5/6 md:w-1/5 mt-4 md:m-4 cursor-pointer"
+      v-for="i in (items = 15)"
+      :key="i"
+      @click="moveTo(i)"
+    >
+      <img
+        class="relative z-10 w-full max-h-80 object-cover"
+        src="https://via.placeholder.com/300x400"
+        alt=""
+      />
+      <div class="absolute z-20 top-0 w-full h-full">
+        <p
+          class="absolute z-20 w-full bottom-0 bg-black bg-opacity-30 p-2 text-xl font-bold"
+        >
+          TEST EVENT
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang='ts'>
 import { Component, Vue, Prop } from "nuxt-property-decorator";
 
 @Component
-export default class index extends Vue {}
+export default class index extends Vue {
+  moveTo(n: number): void {
+    this.$router.push("/events/" + n);
+  }
+}
 </script>
 
 <style scoped lang='postcss'>
